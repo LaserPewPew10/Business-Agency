4a494935f29dc2dadfa88c7a8a2ba38df9ddfb87
Justin Ramirez <ramirezbjustin@gmail.com>
2021-04-16 15:57:14 -0400

    added in pause and play functionlaity of video

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ menu.addEventListener('click', () => {
   menu.classList.toggle('change');
 });
 // End of NavBar
+
+// section 2 video
+const video = document.querySelector('.video');
+const btn = document.querySelector('.buttons i');
+const bar = document.querySelector('.video-bar');
+
+const playPause = () => {
+  if (video.paused) {
+    video.play();
+    btn.className = 'far fa-pause-circle';
+  } else {
+    video.pause();
+    btn.className = 'far fa-play-circle';
+  }
+};
+
+btn.addEventListener('click', () => {
+  playPause();
+});
+//end of section 2 video
