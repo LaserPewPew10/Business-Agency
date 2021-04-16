90366e4ab0cbb9ab8067b70c4c228cb6e498ccb7
Justin Ramirez <ramirezbjustin@gmail.com>
2021-04-16 14:00:23 -0400

    added in dropdown aspect

---
+++ script.js
@@ -0,0 +1,8 @@
+// NavBar
+const menu = document.querySelector('.menu');
+const navbar = document.querySelector('.navbar');
+
+menu.addEventListener('click', () => {
+  navbar.classList.toggle('change');
+});
+// End of NavBar
